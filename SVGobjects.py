0fd2121e0fa2cc8dea0278a1d992116abd225729
Andy Lewis <a.lewis@mathsanswers.org.uk>
2018-06-28 20:34:23 +0100

    Add copyright notice.

--- SVGobjects.py
+++ SVGobjects.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2014 Andy Lewis                                               #
+# Copyright (c) 2014-2018 Andy Lewis                                               #
 # --------------------------------------------------------------------------- #
 # This program is free software; you can redistribute it and/or modify it     #
 # under the terms of the GNU General Public License version 2 as published by #
